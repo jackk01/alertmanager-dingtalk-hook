967bffb6f093e30c526b554bd4bc91426170351d
jackk01 <332578532@qq.com>
2021-06-04 17:36:32 +0800

    gracefully shutdown http server

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/jackk01/alertmanager-dingtalk-hook/model"
 	"github.com/jackk01/alertmanager-dingtalk-hook/pkg"
@@ -20,6 +25,10 @@ func httpHandle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	body, _ := ioutil.ReadAll(r.Body)
+	if len(body) == 0 {
+		fmt.Fprint(w, "")
+		return
+	}
 	log.Print(string(body))
 
 	var notification model.Notification
@@ -38,12 +47,25 @@ func httpHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, `{"message": "send to dingtalk successful"}`)
 }
 
+func serveHTTP(srv *http.Server) {
+	err := srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatalf("listen error, %s", err.Error())
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", httpHandle)
-	server := &http.Server{
-		Addr:    ":5000",
-		Handler: mux,
-	}
-	log.Fatal(server.ListenAndServe())
+	server := &http.Server{Addr: ":5000", Handler: mux}
+	go serveHTTP(server)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	server.Shutdown(ctx)
 }
